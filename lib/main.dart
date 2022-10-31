c5a3d62b0f92c66ff45fbb4e874f6e1a25ae9efb
GameGodS3 <sudevssuresh@gmail.com>
2022-10-31 21:57:25 +0530

    Added: Login UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:live_tracking/screens/map_page.dart';
 import 'package:live_tracking/services/location_service.dart';
 
@@ -17,10 +18,76 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: SafeArea(
+        home: const SafeArea(
           child: Scaffold(
-            body: MapPage(),
+            backgroundColor: Color(0xffe4f7ec),
+            // body: MapPage(),
+            body: LoginPage(),
           ),
         ));
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 120, horizontal: 32),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromRGBO(36, 31, 31, 0.13),
+            spreadRadius: 5,
+            blurRadius: 12,
+            offset: Offset(4, 4),
+          )
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(28),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Mudrika",
+              style: GoogleFonts.lora(fontSize: 48),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "Disaster Management Infrastructure",
+              style: GoogleFonts.yrsa(
+                  fontSize: 14, color: const Color(0xFF33322B)),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF5b5750)),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.wallet),
+                      SizedBox(width: 8),
+                      Text("Sign In with Metamask"),
+                    ],
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
