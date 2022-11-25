14fa04688c3e81ed6079c0e6274067517b32c6d9
GameGodS3 <sudevssuresh@gmail.com>
2022-11-25 10:35:56 +0530

    Added: mobile_scanner package

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:live_tracking/screens/qrscanner_custom.dart';
+import 'package:live_tracking/screens/qrscanner_mobile_scanner.dart';
 // import 'package:live_tracking/screens/qrscanner.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:walletconnect_dart/walletconnect_dart.dart';
---
+++ lib/screens/qrscanner_mobile_scanner.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class QRScanner extends StatefulWidget {
+  const QRScanner({super.key});
+
+  @override
+  State<QRScanner> createState() => _QRScannerState();
+}
+
+class _QRScannerState extends State<QRScanner> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mobile Scanner')),
+      body: MobileScanner(
+          allowDuplicates: false,
+          controller: MobileScannerController(
+              facing: CameraFacing.front, torchEnabled: true),
+          onDetect: (barcode, args) {
+            if (barcode.rawValue == null) {
+              debugPrint('Failed to scan Barcode');
+            } else {
+              final String code = barcode.rawValue!;
+              debugPrint('Barcode found! $code');
+            }
+          }),
+    );
+  }
+}
